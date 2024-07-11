357a689f32bcd97877a6e31eb49bbce3358e22d2
WendelRisse <161463701+WendelRisse@users.noreply.github.com>
2024-07-11 10:13:33 -0300

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,182 @@
+<template>
+
+  <!DOCTYPE html>
+  <html lang="pt-br">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="imagens.css">
+    <title>Imagens</title>
+  </head>
+
+  <body>
+    <h1>RIOS MAIS POLUÍDOS DO MUNDO!</h1>
+    <h2>Veja os impactos</h2>
+    <img src="../assets/Citarum.jpg" alt="motel" class="imagemgrande" width="800" height="400">
+
+    <div class="row">
+      <img src="../assets/rioamarelo.jpg" alt="motel2" width="400" height="400">
+
+      <img src="../assets/rioganges.jpg" alt="motel3" width="400" height="400">
+    </div>
+    <div class="video">
+      <iframe width="800" height="400" src="https://www.youtube.com/embed/nLKv5G1RrZI"
+        title="OS 5 RIOS MAIS POLUÍDOS DO MUNDO" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+    </div>
+    <div class="tabela">
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Nome do Produto</th>
+            <th>Imagem do Produto</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Kit Limpeza</td>
+            <td><img src="../assets/kitlimpeza.jpg" alt="Imagem do Kit de Limpeza"></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Produtos de Limpeza</td>
+            <td><img src="../assets/produtosdelimpeza.jpg" alt="Imagem dos Produtos de Limpeza"></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Produtos Químicos</td>
+            <td><img src="../assets/produtosquimicos.jpg" alt="Imagem do Produtos Químicos
+                        "></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+  </body>
+
+  </html>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 50px;
+}
+
+body {
+  font-family: Arial, sans-serif;
+}
+
+.row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.imagemgrande {
+  margin: auto;
+  display: block;
+  max-width: 100%;
+  height: auto;
+  padding: 1%;
+}
+
+.imagempequena {
+  flex: 100%;
+  padding: 5%;
+}
+
+h1 {
+  text-align: center;
+  font-size: 40px;
+  font-family: Arial, sans-serif;
+  text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+  padding: 10px 20px;
+  border-bottom: 5px solid #ddd;
+}
+
+h2 {
+  font-size: 24px;
+  color: #666;
+  margin-bottom: 1%;
+  text-align: center;
+}
+
+
+@media (max-width: 768px) {
+  .row {
+    flex-direction: column;
+  }
+
+  .imagemgrande {
+    width: 100%;
+  }
+
+  .imagempequena {
+    width: 100%;
+  }
+
+  h1 {
+    font-size: 30px;
+  }
+
+  h2 {
+    font-size: 18px;
+  }
+
+}
+
+.video {
+  padding-top: 1%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+img {
+  border: 1px solid #ccc;
+  padding: 10px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+img:hover {
+  transform: rotate(2deg);
+}
+
+iframe {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  border: 1px solid #ccc;
+  padding: 5px;
+  text-align: left;
+}
+
+th {
+  font-weight: bold;
+}
+
+td img {
+  max-width: 100px;
+}
+</style>
